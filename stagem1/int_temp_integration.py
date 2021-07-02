0d7b668a78951145bee6c9ae2fcea83cc533633e
Margot <margotdemol@gmail.com>
2021-07-02 16:43:35 +0200

    2/07

--- stagem1/int_temp_integration.py
+++ stagem1/int_temp_integration.py
@@ -35,55 +35,163 @@ class Position:
         
         
         
-#VELOCITY
+#VELOCITY FIELD
 def analytical_velocity_advected(t, x, um, uw, w, k):
     return um + uw*np.cos(w*t-k*(x-um*t))
 
 def analytical_velocity_unadvected(t, x, um, uw, w, k):
     return (um + uw*np.cos(w*t-k*x))
 
+
 @xs.process
-class AnaVelocity:
-    """Calculate velocity at t and in all positions.
-    """
+class Velocity_Field:
+    """Initiate the Eulerian velocity field on a grid"""
     
-    # parameters
+    #velocity parameters
     um = xs.variable(description="uniform and constant flow amplitude", attrs={"units":"m/s"})
     uw = xs.variable(description="wave amplitude", attrs={"units":"m/s"})
     w = xs.variable(description="wave pulsation", attrs={"units":"s^-1"})
-    
     k = xs.variable(description="wave number", attrs={"units":"m⁻1"})
     advected = xs.variable(description="advected wave", attrs={"units":"1"})#booléen
     
-    # variables
-    v = xs.variable(dims="a", intent="out", description="velocity of particules", attrs={"units": "m/s", "long_name":"Velocities"})
-    p = xs.foreign(Position, "p", intent="in")
+    #grid parameters
+    t_i = xs.variable(description="starting time", attrs={"units":"s"})
+    t_e = xs.variable(description="ending time", attrs={"units":"s"})
+    t_step = xs.variable(description="time step", attrs={"units":"s"})
+    x_i = xs.variable(description="first limit of positions", attrs={"units":"m"})
+    x_e = xs.variable(description="end limit of positions", attrs={"units":"m"})
+    x_step = xs.variable(description="position step", attrs={"units":"m"})
     
+    #analytical function
     def velocity_func(self, *args):
         if self.advected:
             return analytical_velocity_advected(*args)
         else:
             return analytical_velocity_unadvected(*args)
         
-    #velocity initialisation
+    # variables
+    VF= xs.variable(dims=("t","x"), description="Eulerian velocity field",intent='out')
+    x = xs.index(dims="x")
+    t = xs.index(dims="t")
+
+    
+    @xs.runtime()
+    def initialize(self):
+        self.t=np.arange(self.t_i,self.t_e,self.t_step)
+        self.x=np.arange(self.x_i,self.x_e,self.x_step)
+        #print(self.x[-1], self.t[-1])
+        len_t=len(self.t)
+        len_x=len(self.x)
+        self.VF=np.zeros((len_t, len_x))
+        for i in range(len_t):
+            for j in range (len_x):
+                self.VF[i,j]=analytical_velocity_advected(self.t[i], self.x[j], self.um, self.uw, self.w, self.k)
+    
+                
+#INTERPOLATION FUNCTIONS
+def bilinear_int(p_liste, ts, VF, x, t, t_step, x_step):
+    v=[]
+    it1 = np.searchsorted(t,ts, side='right')-1
+    it2 = it1 + 1
+    #print(it1,it2)
+    if it2>=len(t):
+        print('t out of velocity field :'+ str(ts))
+        return np.zeros_like(p_liste)
+    for p in p_liste:
+        ip1 = np.searchsorted(x,p, side='right')-1
+        if ip1+1>=len(x):
+            print('x out of velocity field')
+            v.append(0)
+        else:
+            ip2 = ip1 + 1
+            #print(ip1,ip2)
+        
+            alpha=p-x[ip1]
+            beta=ts-t[it1]
+        
+    
+            delta_vx = VF[it1,ip2]-VF[it1,ip1]
+            delta_vy = VF[it2,ip1]-VF[it1,ip1]
+            delta_vxy = VF[it1,ip1] + VF[it2,ip2] - VF[it1,ip2] - VF[it2,ip1]
+            v.append(delta_vx*alpha/x_step + delta_vy*beta/t_step + delta_vxy*alpha*beta/x_step/t_step + VF[it1,ip1])
+    return np.array(v)
+
+def lagrange_int(p_liste,ts,VF,x,t,t_step,x_step):
+    v=[]
+    it1 = np.searchsorted(t,ts, side='right')-1
+    it=[it1+o for o in [-1,0,1,2]]
+
+    #print(it1,it2)
+    if it1+1>=len(t) or it1==0:
+        print('t out of velocity field :'+ str(ts))
+        return np.zeros_like(p_liste)
+    for p in p_liste:
+        ip1 = np.searchsorted(x,p, side='right')-1
+        if ip1+1>=len(x) or ip1==0:
+            print('x out of velocity field :'+str(p))
+            v.append(0)
+        else:
+            ip=[ip1+o for o in [-1,0,1,2]]
+            v_sum=0
+            for i in range(4):
+                for j in range(4):
+                    prod1=1
+                    prod2=1
+                    prod3=1
+                    prod4=1
+                    for r in range(4):
+                        if r!=i:
+                            prod1*=p-x[ip[r]]
+                            prod2*=x[ip[i]]-x[ip[r]]
+                        if r!=j:
+                            prod3*=ts-t[it[r]]
+                            prod4*=t[it[j]]-t[it[r]]
+                    v_sum+=VF[it[j], ip[i]]*(prod1*prod3)/(prod2*prod4)
+            v.append(v_sum)
+    return np.array(v)
+
+#VELOCITY
+@xs.process
+class Velocity:
+    """Compute the evolution of positions"""
+    
+    inter_method = xs.variable(description="Interpolation_method")
+    v = xs.variable(dims="a", intent="out", description="velocity of particules", attrs={"units": "m"})
+    
+    VF = xs.foreign(Velocity_Field, 'VF')
+    t = xs.foreign(Velocity_Field, 't')
+    x = xs.foreign(Velocity_Field, 'x')
+    t_step = xs.foreign(Velocity_Field, 't_step')
+    x_step = xs.foreign(Velocity_Field, 'x_step')
+    p = xs.foreign(Position, 'p')
+     
+    #interpolation function
+    def interpolation_func(self,p, ts):
+        if self.inter_method == 'bilinear':
+            return bilinear_int(p, ts, self.VF, self.x, self.t, self.t_step, self.x_step)
+        if self.inter_method == 'lagrange':
+            return lagrange_int(p, ts, self.VF, self.x, self.t, self.t_step, self.x_step)
+        
+    
+    #INITIALISATION OF VELOCITY
     @xs.runtime(args="sim_start") #this way t is the beginning time of the simulation
-    def initialize(self,t):
-        self.v = self.velocity_func(t, self.p, self.um, self.uw, self.w, self.k)
-    
-    
+    def initialize(self,ts):
+        self.v = self.interpolation_func(self.p, ts)
+
+         
     @xs.runtime(args=["step_start"])
-    def run_step(self, t):
-         self.v = self.velocity_func(t, self.p, self.um, self.uw, self.w, self.k)  # v(x(t),t)
+    def run_step(self, ts):
+        self.v = self.interpolation_func(self.p, ts)# v(x(t),t)
+        if len(self.v)==50:
+            print(ts)
         
         
-    #velocity t time and position p
+    #CALCULATION OF VELOCITY AT t time and position p
     @xs.runtime(args=["step_end"])
-    def finalize_step(self, t):
-        self.v = self.velocity_func(t, self.p, self.um, self.uw, self.w, self.k)  # v(x(t+dt),t+dt)
-        
-    
-    
-    
+    def finalize_step(self, te):
+        self.v = self.interpolation_func(self.p, te) # v(x(t+dt),t+dt)
+                        
+            
 #POSITIONS REGULAR INITIALISATION 
 @xs.process
 class InitPRegular:
@@ -109,7 +217,8 @@ class Euler:
     """Calculate positions at t+dt using Euler method.
 """
     p_advected = xs.variable(dims="a", intent="out", groups="p_vars")
-    v = xs.foreign(AnaVelocity, "v", intent="in")
+    v = xs.foreign(Velocity, "v", intent="in")
+
 
     @xs.runtime(args="step_delta")
     def run_step(self, dt):
@@ -122,28 +231,29 @@ class Runge_Kutta2:
     """Calculate positions at t+dt using Runge-Kutta method of order 2.
 """
     p_advected = xs.variable(dims="a", intent="out", groups="p_vars")
+    
 
-    v = xs.foreign(AnaVelocity, "v", intent="in")
+    inter_method = xs.foreign(Velocity, "inter_method", intent="in")
+    v = xs.foreign(Velocity, "v", intent="in")
     p = xs.foreign(Position, "p", intent="in")
-    
-    #parameters
-    advected = xs.foreign(AnaVelocity, "advected", intent="in")
-    um = xs.foreign(AnaVelocity, "um")
-    uw = xs.foreign(AnaVelocity, "uw")
-    w = xs.foreign(AnaVelocity, "w")
-    k = xs.foreign(AnaVelocity, "k")
-    
-    def velocity_func(self, *args):
-        if self.advected:
-            return analytical_velocity_advected(*args)
-        else:
-            return analytical_velocity_unadvected(*args)
-    
+    VF = xs.foreign(Velocity_Field, 'VF', intent='in')
+    t_step = xs.foreign(Velocity_Field, 't_step')
+    x_step = xs.foreign(Velocity_Field, 'x_step')
+    t = xs.foreign(Velocity_Field, 't')
+    x = xs.foreign(Velocity_Field, 'x')
+
+    #interpolation function
+    def interpolation_func(self,*args):
+        if self.inter_method == 'bilinear':
+            return bilinear_int(*args)
+        if self.inter_method == 'lagrange':
+            return lagrange_int(*args)
+        
+        
     @xs.runtime(args=["step_delta", "step_start"])
-    def run_step(self, dt,t):
+    def run_step(self, dt,ts):
         self._p1 =self.p + self.v*dt/2
-        self.p_advected = dt*self.velocity_func(t+dt/2, self._p1, self.um, self.uw, self.w, self.k)
-        #self.p_advected = dt/2*(self.velocity_func(t+dt, self._p1, self.um, self.uw, self.w, self.k)+self.velocity_func(t, self.p, self.um, self.uw, self.w, self.k))
+        self.p_advected = dt*self.interpolation_func(self._p1,ts+dt/2,self.VF,self.x,self.t, self.t_step, self.x_step)
 
 #RUNGE KUTTA 2
 @xs.process
@@ -151,28 +261,28 @@ class Runge_Kutta2_1:
     """Calculate positions at t+dt using Runge-Kutta method of order 2.
 """
     p_advected = xs.variable(dims="a", intent="out", groups="p_vars")
-
-    v = xs.foreign(AnaVelocity, "v", intent="in")
+    
+    inter_method = xs.foreign(Velocity, "inter_method", intent="in")
+    v = xs.foreign(Velocity, "v", intent="in")
     p = xs.foreign(Position, "p", intent="in")
-    
-    #parameters
-    advected = xs.foreign(AnaVelocity, "advected", intent="in")
-    um = xs.foreign(AnaVelocity, "um")
-    uw = xs.foreign(AnaVelocity, "uw")
-    w = xs.foreign(AnaVelocity, "w")
-    k = xs.foreign(AnaVelocity, "k")
-    
-    def velocity_func(self, *args):
-        if self.advected:
-            return analytical_velocity_advected(*args)
-        else:
-            return analytical_velocity_unadvected(*args)
-    
+    VF = xs.foreign(Velocity_Field, 'VF', intent='in')
+    t_step = xs.foreign(Velocity_Field, 't_step')
+    x_step = xs.foreign(Velocity_Field, 'x_step')
+    t = xs.foreign(Velocity_Field, 't')
+    x = xs.foreign(Velocity_Field, 'x')
+
+    #interpolation function
+    def interpolation_func(self,*args):
+        if self.inter_method == 'bilinear':
+            return bilinear_int(*args)
+        if self.inter_method == 'lagrange':
+            return lagrange_int(*args)
+        
+        
     @xs.runtime(args=["step_delta", "step_start"])
-    def run_step(self, dt,t):
+    def run_step(self, dt,ts):
         self._p1 =self.p + self.v*dt/2
-        #self.p_advected = dt*self.velocity_func(t+dt/2, self._p1, self.um, self.uw, self.w, self.k)
-        self.p_advected = dt/2*(self.velocity_func(t+dt, self._p1, self.um, self.uw, self.w, self.k)+self.velocity_func(t, self.p, self.um, self.uw, self.w, self.k))
+        self.p_advected = dt/2*(self.v + self.interpolation_func(self._p1,ts,self.VF,self.x,self.t, self.t_step, self.x_step))
         
         
 #RUNGE KUTTA 4
@@ -181,39 +291,41 @@ class Runge_Kutta4:
     """Calculate positions at t+dt using Runge-Kutta method of order 4.
 """
     p_advected = xs.variable(dims="a", intent="out", groups="p_vars")
-
-    v = xs.foreign(AnaVelocity, "v", intent="in")
+    
+    inter_method = xs.foreign(Velocity, "inter_method", intent="in")
+    v = xs.foreign(Velocity, "v", intent="in")
     p = xs.foreign(Position, "p", intent="in")
+    VF = xs.foreign(Velocity_Field, 'VF', intent='in')
+    t_step = xs.foreign(Velocity_Field, 't_step')
+    x_step = xs.foreign(Velocity_Field, 'x_step')
+    t = xs.foreign(Velocity_Field, 't')
+    x = xs.foreign(Velocity_Field, 'x')
     
-    #parameters
-    advected = xs.foreign(AnaVelocity, "advected", intent="in")
-    um = xs.foreign(AnaVelocity, "um")
-    uw = xs.foreign(AnaVelocity, "uw")
-    w = xs.foreign(AnaVelocity, "w")
-    k = xs.foreign(AnaVelocity, "k")
 
-    def velocity_func(self, *args):
-        if self.advected:
-            return analytical_velocity_advected(*args)
-        else:
-            return analytical_velocity_unadvected(*args)
-    
+    #interpolation function
+    def interpolation_func(self,*args):
+        if self.inter_method == 'bilinear':
+            return bilinear_int(*args)
+        if self.inter_method == 'lagrange':
+            return lagrange_int(*args)
+        
+
     @xs.runtime(args=["step_delta", "step_start"])
-    def run_step(self, dt,t):
+    def run_step(self, dt,ts):
         
-        t2=t+dt/2
+        t2=ts+dt/2
 
-        self._p1 = self.p + dt/2 * self.v
-        self._v1 = self.velocity_func(t2, self._p1, self.um, self.uw, self.w, self.k)# v(p1, t+dt/2)
+        self._p1 = self.p +  self.v*dt/2 
+        self._v1 = self.interpolation_func(self._p1, t2,self.VF,self.x,self.t, self.t_step, self.x_step)# v(p1, t+dt/2)
+
+        self._p2 = self.p +self._v1*dt/2 
+        self._v2 = self.interpolation_func(self._p2, t2,self.VF,self.x,self.t, self.t_step, self.x_step)# v(p2, t+dt/2)
+
+        self._p3 = self.p + self._v2*dt 
+        self._v3 = self.interpolation_func(self._p3, ts+dt,self.VF,self.x,self.t, self.t_step, self.x_step)# v(p3, t+dt)
+
         
-        self._p2 = self.p + dt/2 * self._v1 
-        self._v2 = self.velocity_func(t2, self._p2, self.um, self.uw, self.w, self.k)# v(p2, t+dt/2)
-        
-        self._p3 = self.p + dt * self._v2
-        self._v3 = self.velocity_func(t+dt, self._p3, self.um, self.uw, self.w, self.k)# v(p3, t+dt)
-        
-        
-        self.p_advected = dt/6 *( self.v + 2*self._v2 + self._v3 + 2*self._v1)
+        self.p_advected = (self.v + self._v2*2. + self._v3 + self._v1*2.)*dt/6
 
         
         
@@ -225,22 +337,27 @@ class SetUp:
                  time= list(np.arange(0,d2s*6, h2s)),
                  otime=list(np.arange(0, d2s*6-h2s, h2s)),
                  init_mini=0, init_maxi=200*km, init_N=100,
-                 um=0.1, uw=0.1, w=w2, k=k2, advected=1):
+                 um=0.1, uw=0.1, w=w2, k=k2, advected=1,
+                inter_method='bilinear',
+                t_i=-3600/5,t_e=10*d2s,t_step=3600/5,
+                x_i=-1*km,x_e=5*L,x_step=1*km):
 
         self.model= xs.Model({
+            "v_field": Velocity_Field,
             "position": Position,
-            "init": init_p,
+            "init": InitPRegular,
             "intmethod": intmethod,
-            "velocity": AnaVelocity
+            "velocity": Velocity
         })
         self.in_ds=xs.create_setup(model=self.model,
-                            clocks={'time': time,
-                                    'otime': otime}, 
-                        master_clock='time',
-                        input_vars={'init': {'mini': init_mini, 'maxi':init_maxi, 'N':init_N},
-                                    'velocity': {'um': um, 'uw': uw, 'w':w, 'k':k, 'advected':advected},
+                      clocks={'time': time,
+                                    'otime': otime, 'vf':[0]},
+                      master_clock='time',
+                      input_vars={'init': {'mini': init_mini, 'maxi':init_maxi, 'N':init_N},
+                                    'v_field': {'um': um, 'uw': uw, 'w':w, 'k':k, 'advected':advected, 't_i':t_i, 't_e':t_e, 't_step':t_step, 'x_i':x_i, 'x_e':x_e, 'x_step':x_step},
+                                  'velocity':{'inter_method': inter_method}
                                     },
-                        output_vars={'position__p' : 'otime','velocity__v' : 'otime'})
+                        output_vars={'position__p' : 'otime', 'velocity__v':'otime', 'v_field__VF':'vf'})
         
         self.out_ds=self.in_ds.xsimlab.run(model=self.model)
         self.add_()
@@ -257,6 +374,7 @@ class SetUp:
         
         self.out_ds['displacement_km'] = (self.out_ds.position__p-self.out_ds.position__p.isel(otime=0))/km
         self.out_ds.displacement.attrs={"units":"km", "long_name":"Displacement"}
+
         
         otime_day = self.out_ds.otime/(24*3600)
 
@@ -264,6 +382,11 @@ class SetUp:
         self.out_ds.otime_day.attrs={"units":"day", "long_name":"Time"}
         
         self.out_ds.a.attrs={"units":"m", "long_name":"Particule initial position"}
+        
+        self.out_ds['velocity_field']=self.out_ds.v_field__VF.isel(vf=0)
+        self.out_ds=self.out_ds.drop('v_field__VF').drop('vf')
+        self.out_ds.x.attrs={"units":"m", "long_name":"Position"}
+        self.out_ds.t.attrs={"units":"s", "long_name":"Time"}
 
 
     
@@ -281,24 +404,25 @@ class SetUp:
         if item=='otime':
             return self.out_ds.otime
         if item=='um':
-            return float(self.out_ds.velocity__um)
+            return float(self.out_ds.v_field__um)
         if item=='uw':
-            return float(self.out_ds.velocity__uw)
+            return float(self.out_ds.v_field__uw)
         if item=='w':
-            return float(self.out_ds.velocity__w)
+            return float(self.out_ds.v_field__w)
         if item=='k':
-            return float(self.out_ds.velocity__k)
+            return float(self.out_ds.v_field__k)
         if item=='advected':
-            return float(self.out_ds.velocity__advected)
+            return float(self.out_ds.v_field__advected)
         
     def update_intmethod(self,intmethod):#update processes of the model ex: change Euler->Runge Kutta: **process = intmethod=Runge_Kutta2
         
         #self.model = (self.model).update_processes(process)
         self.model= xs.Model({
+            "v_field":Velocity_Field,
             "position": Position,
             "init": InitPRegular,
             "intmethod": intmethod,
-            "velocity": AnaVelocity
+            "velocity": Velocity
         })
         #CAUTION: CREATE NEW MODEL (PROBLEM WITH P_VARS OTHERWISE)
 
